c916e57760476d8263eb8f77b68d90e7852ce3be
camilo cabana <camilo_11_04@hotmail.com>
2020-05-26 13:56:41 +0900

    Initial Commit

---
+++ ARTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARTest
+//
+//  Created by Camilo Cabana on 26/05/20.
+//  Copyright © 2020 Camilo Cabana. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
