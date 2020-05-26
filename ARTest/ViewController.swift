364d41e2888473157cbc5babd7acdd78360abb67
camilo cabana <camilo_11_04@hotmail.com>
2020-05-26 17:31:47 +0900

    AR test

--- ARTest/ViewController.swift
+++ ARTest/ViewController.swift
@@ -12,14 +12,41 @@ import RealityKit
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
+    var testAnchor: Test.RoboTest!
+    var planeAnchor: Test1.Plane!
+    let startButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
+        testAnchor = try! Test.loadRoboTest()
+        planeAnchor = try! Test1.loadPlane()
+        // push AR buttons
+        testAnchor.generateCollisionShapes(recursive: true)
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.scene.anchors.append(testAnchor)
+        arView.scene.anchors.append(planeAnchor)
+        //..
+        setStartButton()
+    }
+    
+    func setStartButton() {
+        startButton.frame = CGRect(x: view.center.x - 50, y: -50, width: 100, height: 50)
+        view.addSubview(startButton)
+        startButton.layer.cornerRadius = 25
+        startButton.backgroundColor = .white
+        startButton.setTitle("Call Friend", for: .normal)
+        startButton.setTitleColor(.systemBlue, for: .normal)
+        UIView.animate(withDuration: 0.3) {
+            self.startButton.frame = CGRect(x: self.view.center.x - 75, y: 50, width: 150, height: 50)
+        }
+        startButton.addTarget(self, action: #selector(startARTest), for: .touchUpInside)
+    }
+    
+    @objc func startARTest(_ sender: UIButton) {
+        //call the animation
+        planeAnchor.notifications.plane.post()
+        
     }
 }
